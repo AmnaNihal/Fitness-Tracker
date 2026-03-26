acefa3ceef0361543894106ecac595729bb02d9d
Amna <amnanihal2002@gmail.com>
2026-03-27 01:35:12 +0500

    Delete backend directory

--- backend/routes/logRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createLog, getLogs,clearLogs } = require('../controllers/logController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.route('/').post(protect, createLog).get(protect, getLogs) .delete(protect, clearLogs);
-
-module.exports = router;
